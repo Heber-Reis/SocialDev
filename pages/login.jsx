939cdc22775abc00043b0bf2d92194efd72f62e7
Heber Magno <heber2014gbi@gmail.com>
2022-03-05 19:56:04 -0300

    criado componente de imagem

---
+++ pages/login.jsx
@@ -0,0 +1,11 @@
+import ImageWithSpace from '../src/components/layout/ImageWIthSpace';
+
+function LoginPage () {
+  return(
+    <ImageWithSpace>
+      Teste
+    </ImageWithSpace>
+  )
+}
+
+export default LoginPage;
