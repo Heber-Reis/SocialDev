5d7941c4fda3b8760ef2a3d116124e6b35b9175e
Heber Magno <heber2014gbi@gmail.com>
2022-04-11 23:09:20 -0300

    criado cadastro de user

--- modules/user/user.service.js
+++ modules/user/user.service.js
@@ -1,5 +1,23 @@
-export const signupUser = (body) => {
-  console.log('SIGNUP')
-  console.log(body)
-  return true
+import { hashPassword } from "../../utils/bcrypt"
+
+import User from './user.model'
+
+export const signupUser = async (body) => {
+
+  //criando um objeto para enviar ao BD, copiando todos os dados da requisição e modificando o atributo senha 
+  try {
+    const user = {
+      ...body,
+      password: hashPassword(body.password)
+    }
+    const dbUser  = await User.create(user)
+    return dbUser
+  } catch (err) {
+    console.error(err)
+    throw err
+  }
+
+
+  console.log(user)
+  
 }
--- pages/api/user/signup.js
+++ pages/api/user/signup.js
@@ -17,10 +17,16 @@ const postSchema = Joi.object({
 const signup = createHandler()
 //função validate => estamos dizendo que vamos validar o body com as validações do postSchema
 //só entra na função da requisição se todos os dados estiverem válidos
-signup.post(validate({body: postSchema}),(req,res) => {
-  console.log("Entrou requisição")
-  signupUser(req.body)
-  res.status(200).json({ ligar: false })
+signup.post(validate({body: postSchema}), async (req,res) => {
+  try{
+    console.log("Entrou requisição")
+    const user = await signupUser(req.body)
+    res.status(201).json(user)
+  }
+  catch (err){
+    console.error(err)
+    throw err
+  }
 })
 
 export default signup;
